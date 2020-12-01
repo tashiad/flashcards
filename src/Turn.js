097a787d0fdf30885509eeee20dc12c4e73aa6fc
Tashia Davis <tashiadavis10@gmail.com>
2020-11-30 18:15:30 -0700

    Add Turn class outline

---
+++ src/Turn.js
@@ -0,0 +1,25 @@
+class Turn {
+  constructor(guess, card) {
+    this.guess = guess;
+    this.card = card;
+  }
+
+  returnGuess() {
+
+  }
+
+  returnCard() {
+
+  }
+
+  evaluateGuess() {
+
+  }
+
+  giveFeedback() {
+    
+  }
+
+}
+
+module.exports = Turn;
