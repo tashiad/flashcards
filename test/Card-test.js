63224cd9d3a7cec865e816ae9d27b7b4d9cce712
Tashia Davis <tashiadavis10@gmail.com>
2020-12-03 16:20:19 -0700

    Finalize Card and test refactor and edits

--- test/Card-test.js
+++ test/Card-test.js
@@ -2,34 +2,35 @@ const chai = require('chai');
 const expect = chai.expect;
 const Card = require('../src/Card');
 
-describe('Card', function() {
+describe('Card', () => {
   let card;
 
-  beforeEach(function() {
+  beforeEach( () => {
     card = new Card(1, 'What sound do dogs make?', ['moo', 'woof', 'meow'], 'woof');
   });
 
-  it('should be a function', function() {
+  it('should be a function', () => {
     expect(Card).to.be.a('function');
   });
 
-  it('should be an instance of Card', function() {
+  it('should be an instance of Card', () => {
     expect(card).to.be.an.instanceof(Card);
   });
 
-  it('should have an id', function() {
+  it('should have an id', () => {
     expect(card.id).to.equal(1);
   });
 
-  it('should store a question', function() {
+  it('should store a question', () => {
     expect(card.question).to.equal('What sound do dogs make?');
   });
 
-  it('should store a list of possible answers', function() {
+  it('should store a list of possible answers', () => {
     expect(card.answers).to.deep.equal(['moo', 'woof', 'meow']);
   });
 
-  it('should store the correct answer', function() {
+  it('should store the correct answer', () => {
     expect(card.correctAnswer).to.equal('woof');
   });
+
 });
